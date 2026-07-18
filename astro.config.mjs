7297bdb3c2dbb79b97c1aa194a7dcc83e1455172
Phill Johntony <pdjohntony@gmail.com>
2026-07-18 18:28:52 -0400

    Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), sitemap()],
   site: 'https://philljohntony.com',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
